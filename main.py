583ecf671e88de69cab1cf4c1a3b68b7eebb9b93
Cilantro-byte <113932327+Cilantro-byte@users.noreply.github.com>
2022-09-26 18:08:29 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# Author: Clinton Cargill
-# GitHub username: Cilantro-byte
-# Date: 11/2/1992
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
